ef4962fc2d39088205eb0590bea8e58b1fb08d9e
tiber228 <79192100+tiber228@users.noreply.github.com>
2021-03-24 19:10:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,23 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
+  int l = 0, r = size - 1, mid, numb =0;
+  while = (l+r) {
+    mid = (l+r) / 2;
+    if (arr[mid] < value)
+      l = mid + 1;
+    else
+      r = mid;
+  }
+  if (arr[l] == value) {
+    while (arr[l] == value) {
+      numb++;
+      l++;
+    }
+  }
+  if (numb)
+    return numb
+    else
+ 
   return 0; // если ничего не найдено
 }
