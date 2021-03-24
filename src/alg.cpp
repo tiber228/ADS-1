3058ef09c4f1a78118d4efdbacebac7d85d8634c
tiber228 <79192100+tiber228@users.noreply.github.com>
2021-03-24 18:55:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,24 +2,3 @@
 
 int cbinsearch(int *arr, int size, int value) {
  
-        int i = 0;
-        int j = size - 1;
-        int k = 0;
-                while (i < j) {
-        int center = (i + j) / 2;
-                if (arr[center] < value)
-        i = center + 1;
-                else
-        j = center;
-                if (arr[i] == value) {
-                while (arr[i] == value) {
-        k++;
-        j++;
-                }
-        }
-        }
-        if (k!=0)
-        return k;
-        else
-  return 0; // если ничего не найдено
-}
