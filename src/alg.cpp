604afceb422873cea86194ac6c21b1958531dc0e
tiber228 <79192100+tiber228@users.noreply.github.com>
2021-03-24 18:59:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,4 +1,24 @@
 // Copyright 2021 NNTU-CS
 
-int cbinsearch(int *arr, int size, int value) {
+iint cbinsearch(int *arr, int size, int value) {
+    int r = 0, l = size - 1, k = 0;
+    while (r < l) {
+        
+        int mid = (r + l) / 2;
+        if (arr[mid] < value)
+            r = mid + 1;
+        else
+            l = mid;
+    }
+    if (arr[r] == value) {
+        while (arr[r] == value) {
+            k++;
+            r++;
+        }
+    }
+    if (k != 0)
+        return k;
+    else
+        return 0;
+}
  
